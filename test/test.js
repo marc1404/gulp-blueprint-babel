9b48a65456974e835417306df90aa7eef4088614
Cark <mail@cark.me>
2015-09-17 09:25:22 +0200

    Enhance test

--- test/test.js
+++ test/test.js
@@ -1,3 +1,21 @@
-var babel = require('../lib/gulp-blueprint-babel');
+require('should');
 
+var gulp = require('gulp');
+var babel = require('../lib/gulp-blueprint-babel');
+var sinon = require('sinon');
+var clean = sinon.spy();
+
+gulp.task('clean', clean);
 babel();
+
+describe('when running the babel task', function(){
+
+  it('should complete without an error', function(){
+    gulp.start('babel');
+  });
+
+  it('should have called clean() exactly once', function(){
+    clean.calledOnce.should.be.exactly(true);
+  });
+
+});
