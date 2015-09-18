cfc41ce469b17414283ff77afb288b6579dde8f5
Cark <mail@cark.me>
2015-09-18 10:39:43 +0200

    Use browserify basedir instead of baseDir

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -1,3 +1,10 @@
 var babel = require('../lib/gulp-blueprint-babel');
 
-babel({ dependencies: [] });
+babel({
+    dependencies: [],
+    input: 'app.js',
+    browserify: {
+        debug: true,
+        basedir: 'app/client'
+    }
+});
